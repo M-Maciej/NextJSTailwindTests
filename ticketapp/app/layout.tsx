31c33e619881f6e4f47e2f623ed734895f315592
M-Maciej <130112810+M-Maciej@users.noreply.github.com>
2024-02-17 23:09:24 +0100

    Base navbar complete

--- ticketapp/app/layout.tsx
+++ ticketapp/app/layout.tsx
@@ -1,34 +1,34 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import MainNav from "@/components/MainNav";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import MainNav from "@/components/MainNav"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Ticket Application",
-  description: "Project from Clarity Coders' course",
-};
+	title: "Ticket Application",
+	description: "Project from Clarity Coders' course",
+}
 
 
 export default function RootLayout({
 
-  children,
+	children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <nav className="flex flex-col items-center border-b">
-          <div className="max-w-6xl w-full">
-            <MainNav />
-          </div>
-        </nav>
-        <main className="flex flex-col items-center border-b">
-          <div className="max-w-6xl w-full">{children}</div>
-        </main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
+					<div className="max-w-6xl w-full">
+						<MainNav />
+					</div>
+				</nav>
+				<main className="flex flex-col items-center">
+					<div className="max-w-6xl w-full">{children}</div>
+				</main>
+			</body>
+		</html>
+	)
 }
